550762df771cfe293d4d35a857f5fa90f5592bda
Minh-Anh Nguyen <nguyenminhanh8688@gmail.com>
2026-05-04 23:57:32 +0700

    Fix config files to .mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,8 @@
-import type { Config } from 'postcss-load-config'
-
 const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-} satisfies Config
+}
 
 export default config
